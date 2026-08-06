9c5e6ee52ab2f0b42398ff005fb17e2f100e85ac
Vijaykumar-03 <paxsaat03@gmail.com>
2026-08-06 15:54:26 +0530

    Create main.tf

---
+++ modules/eks/main.tf
@@ -0,0 +1,37 @@
+module "eks" {
+
+  source = "terraform-aws-modules/eks/aws"
+
+  version = "~>20.0"
+
+  cluster_name = "devops-eks"
+
+  cluster_version = "1.33"
+
+  subnet_ids = var.subnet_ids
+
+  vpc_id = data.aws_vpc.default.id
+
+  eks_managed_node_groups = {
+
+    worker = {
+
+      instance_types = ["t3.micro"]
+
+      desired_size = 2
+
+      min_size = 1
+
+      max_size = 1
+
+    }
+
+  }
+
+}
+
+data "aws_vpc" "default" {
+
+  default = true
+
+}
