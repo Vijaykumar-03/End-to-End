2fc840e582101507b4df386408e4f708e0ee6878
Vijaykumar-03 <paxsaat03@gmail.com>
2026-08-06 16:15:10 +0530

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -2,13 +2,22 @@ data "aws_ami" "ubuntu" {
 
   most_recent = true
 
-  owners = ["732231074251"]
+  owners = ["099720109477"]   # Canonical Ubuntu
 
   filter {
-    name = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-jammy-22.04-amd64-server-*"]
   }
 
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
 }
 
 resource "aws_security_group" "ec2" {
@@ -16,51 +25,36 @@ resource "aws_security_group" "ec2" {
   name = "ec2-sg"
 
   ingress {
-
-    from_port = 22
-
-    to_port = 22
-
-    protocol = "tcp"
-
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-
   }
 
   ingress {
-
-    from_port = 80
-
-    to_port = 80
-
-    protocol = "tcp"
-
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-
   }
 
   egress {
-
-    from_port = 0
-
-    to_port = 0
-
-    protocol = "-1"
-
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
-
   }
 
+  tags = {
+    Name = "ec2-sg"
+  }
 }
 
 resource "aws_instance" "server" {
 
-  ami = data.aws_ami.ubuntu.id
-
-  instance_type = "t3.micro"
-
-  subnet_id = var.subnet_id
-
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = "t3.micro"
+  subnet_id                   = var.subnet_id
   associate_public_ip_address = true
 
   vpc_security_group_ids = [
@@ -70,5 +64,4 @@ resource "aws_instance" "server" {
   tags = {
     Name = "Terraform-EC2"
   }
-
 }
