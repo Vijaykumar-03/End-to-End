e363f572dac479243b0ee700b31d62f765e94b5b
Vijaykumar-03 <paxsaat03@gmail.com>
2026-08-06 15:59:54 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,27 +1,56 @@
-variable "region" {
-  default = "ap-south-2"
+variable "aws_region" {
+  description = "AWS Region"
+  type        = string
+  default     = "ap-south-2"
 }
 
-variable "cluster_name" {
-  default = "devops-eks"
+variable "ecr_repository_name" {
+  description = "ECR Repository Name"
+  type        = string
+  default     = "devops-ecr"
 }
 
-variable "repository_name" {
-  default = "devops-ecr"
+variable "ec2_instance_type" {
+  description = "EC2 Instance Type"
+  type        = string
+  default     = "t3.micro"
 }
 
-variable "instance_type" {
-  default = "t3.micro"
+variable "eks_cluster_name" {
+  description = "EKS Cluster Name"
+  type        = string
+  default     = "devops-eks"
 }
 
-variable "key_name" {}
-
-variable "public_subnets" {
-  type = list(string)
+variable "eks_cluster_version" {
+  description = "EKS Version"
+  type        = string
+  default     = "1.33"
 }
 
-variable "private_subnets" {
-  type = list(string)
+variable "node_instance_type" {
+  description = "EKS Worker Node Instance Type"
+  type        = string
+  default     = "t3.micro"
 }
 
-variable "vpc_id" {}
+variable "desired_size" {
+  type    = number
+  default = 2
+}
+
+variable "min_size" {
+  type    = number
+  default = 1
+}
+
+variable "max_size" {
+  type    = number
+  default = 1
+}
+
+variable "lambda_function_name" {
+  description = "Lambda Function Name"
+  type        = string
+  default     = "user-registration"
+}
