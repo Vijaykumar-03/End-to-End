b8d4e884c82b464e97f4f84ff618c5cef5d0aafd
Vijaykumar-03 <paxsaat03@gmail.com>
2026-08-07 15:36:59 +0530

    Update main.tf

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -1,51 +1,37 @@
-resource "aws_iam_role" "lambda" {
-
+resource "aws_iam_role" "lambda_role" {
   name = "lambda-role"
 
   assume_role_policy = jsonencode({
-
     Version = "2012-10-17"
 
     Statement = [
-
       {
-
         Effect = "Allow"
 
         Principal = {
-
           Service = "lambda.amazonaws.com"
-
         }
 
         Action = "sts:AssumeRole"
-
       }
-
     ]
-
   })
-
 }
 
-resource "aws_iam_role_policy_attachment" "basic" {
-
-  role = aws_iam_role.lambda.name
-
+resource "aws_iam_role_policy_attachment" "basic_execution" {
+  role       = aws_iam_role.lambda_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-
 }
 
 resource "aws_lambda_function" "lambda" {
 
   function_name = "user-registration"
 
-  filename = "${path.root}/lambda/lambda.zip"
+  filename         = "${path.module}/lambda.zip"
+  source_code_hash = filebase64sha256("${path.module}/lambda.zip")
 
   handler = "lambda.lambda_handler"
-
   runtime = "python3.12"
 
-  role = aws_iam_role.lambda.arn
-
+  role = aws_iam_role.lambda_role.arn
 }
