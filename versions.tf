0a5a19527a169e66d0b3ccf55ccc80f85f865b91
Vijaykumar-03 <paxsaat03@gmail.com>
2026-08-07 15:45:49 +0530

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,10 +1,8 @@
 terraform {
-  required_version = ">=1.5"
-
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = "~>5.50"
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
     }
   }
 }
