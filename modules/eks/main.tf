20080e522d3486c15ea2f33b6ad8221d10f66b64
Vijaykumar-03 <paxsaat03@gmail.com>
2026-08-07 15:40:52 +0530

    Update main.tf

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -1,37 +1,33 @@
 module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.31"
 
-  source = "terraform-aws-modules/eks/aws"
-
-  version = "~>20.0"
-
-  cluster_name = "devops-eks"
-
+  cluster_name    = "devops-eks"
   cluster_version = "1.33"
 
+  vpc_id     = data.aws_vpc.default.id
   subnet_ids = var.subnet_ids
 
-  vpc_id = data.aws_vpc.default.id
+  enable_cluster_creator_admin_permissions = true
 
   eks_managed_node_groups = {
-
     worker = {
-
       instance_types = ["t3.micro"]
 
-      desired_size = 2
+      min_size     = 1
+      desired_size = 1
+      max_size     = 2
 
-      min_size = 1
-
-      max_size = 3
-
+      capacity_type = "ON_DEMAND"
     }
-
   }
 
+  tags = {
+    Environment = "Dev"
+    Terraform   = "true"
+  }
 }
 
 data "aws_vpc" "default" {
-
   default = true
-
 }
