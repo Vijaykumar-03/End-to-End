6a65a3268acf8f541797733e55521c55ad655e72
Vijaykumar-03 <paxsaat03@gmail.com>
2026-08-07 15:48:35 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,14 +1,11 @@
-##########################################################
-# Terraform
-##########################################################
+provider "aws" {
+  region = var.aws_region
 
-terraform {
-  required_version = ">= 1.5.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 6.0"
+  default_tags {
+    tags = {
+      Project     = "End-to-End"
+      Environment = "Dev"
+      ManagedBy   = "Terraform"
     }
   }
 }
