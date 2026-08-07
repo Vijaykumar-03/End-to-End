841c27943272f72b4a016ca387382edd57406933
Vijaykumar-03 <paxsaat03@gmail.com>
2026-08-07 15:44:25 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,23 +1,51 @@
-output "ec2_ip" {
+##########################################################
+# EC2 Outputs
+##########################################################
 
+output "ec2_instance_id" {
+  value = module.ec2.instance_id
+}
+
+output "ec2_public_ip" {
   value = module.ec2.public_ip
-
 }
 
-output "ecr_url" {
+output "ec2_public_dns" {
+  value = module.ec2.public_dns
+}
 
+##########################################################
+# ECR Outputs
+##########################################################
+
+output "ecr_repository_name" {
+  value = module.ecr.repository_name
+}
+
+output "ecr_repository_url" {
   value = module.ecr.repository_url
-
 }
 
-output "eks_name" {
+##########################################################
+# EKS Outputs
+##########################################################
 
+output "eks_cluster_name" {
   value = module.eks.cluster_name
-
 }
 
-output "lambda_name" {
+output "eks_cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
 
-  value = module.lambda.lambda_name
+##########################################################
+# Lambda Outputs
+##########################################################
 
+output "lambda_function_name" {
+  value = module.lambda.lambda_function_name
+}
+
+output "lambda_arn" {
+  value = module.lambda.lambda_arn
 }
