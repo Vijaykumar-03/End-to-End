f4e2dec289297ce81a14a6e60036b0a3398b364d
Vijaykumar-03 <paxsaat03@gmail.com>
2026-08-07 15:43:50 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,30 @@
+##########################################################
+# Terraform
+##########################################################
+
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+##########################################################
+# AWS Provider
+##########################################################
+
 provider "aws" {
-  region = "ap-south-2"
+  region = var.aws_region
+
+  default_tags {
+    tags = {
+      Project     = "End-to-End"
+      Environment = "Dev"
+      ManagedBy   = "Terraform"
+    }
+  }
 }
